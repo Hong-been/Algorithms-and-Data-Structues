3fa6e467d155af93cc8b8e5072d362d8ad769c97
HongBeen <redbean-2@naver.com>
2021-06-08 23:29:30 +0900

    BOJ 10953-콤마(,)로 구분해서 정수 입력받고 더하기

--- java/algorithm_basic1/100algorithm_start/src/com/company/Main.java
+++ java/algorithm_basic1/100algorithm_start/src/com/company/Main.java
@@ -1,14 +1,19 @@
 package com.company;
 import java.util.*;
 
+import static java.lang.Integer.parseInt;
+
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int T=sc.nextInt();
 
-        while(sc.hasNext()){
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println(a + b);
+        while(T-->0){
+            String s=sc.next();
+            String num[] = s.split(",");
+            int a = parseInt(num[0]);
+            int b = parseInt(num[1]);
+            System.out.println(a+b);
     }}
 }
