9a2b361a82288d2c28282fc2487ff70e47cbff7a
HongBeen <redbean-2@naver.com>
2021-06-08 23:02:57 +0900

    BOJ 10951 - 입력의 갯수가 정해지지 않을 때(EOF)

--- java/algorithm_basic1/100algorithm_start/src/com/company/Main.java
+++ java/algorithm_basic1/100algorithm_start/src/com/company/Main.java
@@ -5,8 +5,8 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t=sc.nextInt();
-        for(int i=0;i<t;i++){
+
+        while(sc.hasNext()){
         int a = sc.nextInt();
         int b = sc.nextInt();
         System.out.println(a + b);
