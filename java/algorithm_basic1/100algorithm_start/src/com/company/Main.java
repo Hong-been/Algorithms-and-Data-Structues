4d2d7a33d175b3060f7c0d91404ac2f9cd247558
HongBeen <redbean-2@naver.com>
2021-06-08 23:43:42 +0900

    BOJ 11021-String.format 사용하여 출력하기

--- java/algorithm_basic1/100algorithm_start/src/com/company/Main.java
+++ java/algorithm_basic1/100algorithm_start/src/com/company/Main.java
@@ -1,19 +1,15 @@
 package com.company;
 import java.util.*;
 
-import static java.lang.Integer.parseInt;
-
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T=sc.nextInt();
 
-        while(T-->0){
-            String s=sc.next();
-            String num[] = s.split(",");
-            int a = parseInt(num[0]);
-            int b = parseInt(num[1]);
-            System.out.println(a+b);
+        for(int i=0;i<T;i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(String.format("Case #%d: ",i+1)+(a+b));
     }}
 }
