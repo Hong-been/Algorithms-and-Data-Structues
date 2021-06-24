5212fc8e0ef9eb74de87b21612f52173cc9f309c
HongBeen <redbean-2@naver.com>
2021-06-24 15:02:03 +0900

    BOJ2004.java 수정 - 예외처리가 틀림, 수정함
    0의 갯수를 출력하는 문제인데, 팩토리얼로 예외처리하여 오답으로 처리되었음.
    예외를 삭제하여 완성함.

--- java/algorithm_basic1/300Math/src/BOJ2004.java
+++ java/algorithm_basic1/300Math/src/BOJ2004.java
@@ -8,14 +8,6 @@ public class BOJ2004 {
         long primeTwo=0;
         long primeFive=0;
 
-        if(m==0 || m==n){
-            System.out.println("1");
-            return;
-        }
-        if(m==1){
-            System.out.println(n);
-            return;
-        }
         primeTwo+= (NumbersPrime(n,2)- NumbersPrime(n-m,2)- NumbersPrime(m,2));
         primeFive+= (NumbersPrime(n,5)- NumbersPrime(n-m,5)- NumbersPrime(m,5));
 
