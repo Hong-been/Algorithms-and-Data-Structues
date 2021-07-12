e54df98ae310a07b00f75918c8ad51f92cdf0aec
HongBeen <redbean-2@naver.com>
2021-07-13 03:32:10 +0900

    BOJ3085.java 생성 - n^2짜리 사탕판에서 두 개를 골라 교환하였을 때, 연속한 칸에서 같은 사탕이 가장 많을 경우를 구하기
    - 정사각형 사탕판에 4가지 종류의 사탕이 무작위로 놓여있을 때
    - 인접한 두개를 골라 자리를 바꾼다.
    - 하나의 행 or 열 내에서 연속한 사탕이 가장 많을 때의 연속한 사탕의 수를 구한다.
    - 사탕을 2개 고르는 모든 경우의 수 O(N^2)
    - 사탕판에서 연속한 사탕의 최대수를 구하는 경우의 수 O(N^2), findMax함수로 찾아낸다.
    - n이 최대 50이고 O(N^4)일때 50^4이므로 모두 계산해도 가능하다.

---
+++ java/algorithm_basic1/500Brute_Force/src/BOJ3085.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+public class BOJ3085 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int max=1;
+        char[][] a = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.next().toCharArray();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == n - 1 && j == n - 1);
+                else if (i == n - 1) {
+                    if (a[i][j] != a[i][j + 1]) {
+                        swap(a,i,j,i,j+1);
+                        int tmp=findMax(a, n);
+                        if(tmp>max) max=tmp;
+                        swap(a,i,j,i,j+1);
+                    }
+                }
+                else if (j == n - 1) {
+                    if (a[i][j] != a[i + 1][j]) {
+                        swap(a,i,j,i+1,j);
+                        int tmp=findMax(a, n);
+                        if(tmp>max) max=tmp;
+                        swap(a,i,j,i+1,j);
+                    }
+                }
+                else {
+                    if (a[i][j] != a[i][j + 1]) {
+                        swap(a,i,j,i,j+1);
+                        int tmp=findMax(a, n);
+                        if(tmp>max) max=tmp;
+                        swap(a,i,j,i,j+1);
+                    }
+                    if (a[i][j] != a[i+1][j]) {
+                        swap(a,i,j,i+1,j);
+                        int tmp=findMax(a, n);
+                        if(tmp>max) max=tmp;
+                        swap(a,i,j,i+1,j);
+                    }
+                }
+            }
+        }
+        System.out.println(max);
+    }
+    static void swap(char [][]a, int i, int j,int x,int y){
+        char tmp = a[i][j];
+        a[i][j] = a[x][y];
+        a[x][y] = tmp;
+    }
+    static int findMax(char[][] a, int n) {
+        int length=1;
+        for(int i=0;i<n;i++){
+            int tmp=1;
+            for(int j=0;j<n-1;j++){
+                if(a[i][j]==a[i][j+1]) tmp+=1;
+                else tmp=1;
+                if(tmp>length) length=tmp;
+            }
+            for(int j=0;j<n-1;j++){
+                if(a[j][i]==a[j+1][i]) tmp+=1;
+                else tmp=1;
+                if(tmp>length) length=tmp;
+            }
+        }
+        return length;
+    }
+}
